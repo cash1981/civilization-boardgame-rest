9c1b7aa04b624facc5e57319c1e034092f6e0822
Shervin Asgari <shervin@asgari.no>
2014-10-23 14:51:30 +0200

    Renamed images to img

--- civilization-web/app/js/app.js
+++ civilization-web/app/js/app.js
@@ -1,12 +1,8 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
+var civApp = angular.module('myApp', ['ngRoute']);
+
+civApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
