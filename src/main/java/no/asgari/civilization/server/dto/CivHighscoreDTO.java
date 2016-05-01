9d1a1d2aae94dbf4b82b49388b6cf3e54c504876
Shervin Asgari <shervin@asgari.no>
2016-05-01 14:16:42 +0200

    Added effieciency in percent

--- src/main/java/no/asgari/civilization/server/dto/CivHighscoreDTO.java
+++ src/main/java/no/asgari/civilization/server/dto/CivHighscoreDTO.java
@@ -1,7 +1,7 @@
 package no.asgari.civilization.server.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +12,7 @@ public class CivHighscoreDTO implements Comparable<CivHighscoreDTO> {
     private String civ;
     private long totalWins;
     private long totalAttempt;
+    private String percentWin;
 
     public CivHighscoreDTO(String civ, Long totalWins, Long totalAttempt) {
         this.civ = civ;
@@ -25,6 +26,7 @@ public class CivHighscoreDTO implements Comparable<CivHighscoreDTO> {
         } else {
             this.totalAttempt = totalAttempt;
         }
+        percentWin = percentWin();
     }
 
     @Override
@@ -35,5 +37,14 @@ public class CivHighscoreDTO implements Comparable<CivHighscoreDTO> {
         }
 
         return civ.compareTo(o.civ);
+    }
+
+    @JsonIgnore
+    private String percentWin() {
+        if(totalWins == 0 || totalAttempt == 0)
+            return "0 %";
+        double percent = ((double)totalWins / (double)totalAttempt) * 100;
+        double twoDecimals = (double) Math.round(percent * 100) / 100;
+        return twoDecimals + " %";
     }
 }
--- src/main/java/no/asgari/civilization/server/dto/WinnerDTO.java
+++ src/main/java/no/asgari/civilization/server/dto/WinnerDTO.java
@@ -1,18 +1,25 @@
 package no.asgari.civilization.server.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class WinnerDTO implements Comparable<WinnerDTO> {
     private String username;
     private int totalWins;
     private long attempts;
+    private String percentWin;
+
+    public WinnerDTO(String username, int totalWins, long attempts) {
+        this.username = username;
+        this.totalWins = totalWins;
+        this.attempts = attempts;
+        this.percentWin = percentWin();
+    }
 
     @Override
     public int compareTo(WinnerDTO o) {
@@ -22,5 +29,14 @@ public class WinnerDTO implements Comparable<WinnerDTO> {
         }
 
         return username.compareTo(o.getUsername());
+    }
+
+    @JsonIgnore
+    private String percentWin() {
+        if(totalWins == 0 || attempts == 0)
+            return "0 %";
+        double percent = ((double)totalWins / (double)attempts) * 100;
+        double twoDecimals = (double) Math.round(percent * 100) / 100;
+        return twoDecimals + " %";
     }
 }
